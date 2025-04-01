c9ff8b2985b72bc24a9a235222bd2a4ce1e0c845
paulmicahsalas <paul_7474@yahoo.com>
2025-04-01 06:40:36 +0000

    Optimize styles - updating docs

--- src/softPurge.ts
+++ src/softPurge.ts
@@ -20,16 +20,16 @@ export async function softPurge({
   key,
   ...swrOverwrites
 }: SoftPurgeOpts) {
-  let swrOverwrite = swrOverwrites.swr ?? swrOverwrites.staleWhileRevalidate;
-  let entry = await getCacheEntry({ cache, key }, () => {});
+  const swrOverwrite = swrOverwrites.swr ?? swrOverwrites.staleWhileRevalidate;
+  const entry = await getCacheEntry({ cache, key }, () => {});
 
   if (entry === CACHE_EMPTY || isExpired(entry.metadata)) {
     return;
   }
 
-  let ttl = entry.metadata.ttl || Infinity;
-  let swr = staleWhileRevalidate(entry.metadata) || 0;
-  let lt = Date.now() - entry.metadata.createdTime;
+  const ttl = entry.metadata.ttl || Infinity;
+  const swr = staleWhileRevalidate(entry.metadata) || 0;
+  const lt = Date.now() - entry.metadata.createdTime;
 
   await cache.set(
     key,
